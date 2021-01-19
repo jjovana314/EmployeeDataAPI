fd2772935bc62e79ece2470cc233166415a0de2d
Jovana Jovanovic <jjovana314@gmail.com>
2021-01-19 16:57:15 +0100

    initial commit

---
+++ web/exception_array.py
@@ -0,0 +1,16 @@
+""" All exception class for project. """
+
+import exceptions
+
+
+schema_exceptions = [
+    exceptions.TypeSchemaError,
+    exceptions.RequiredSchemaError,
+    exceptions.MinLengthSchemaError,
+    exceptions.MaxLengthSchemaError,
+    exceptions.MinimumSchemaError,
+    exceptions.MaximumSchemaError,
+    exceptions.MinimumSchemaError,
+    exceptions.MaximumSchemaError,
+    exceptions.EnumSchemaError
+]
